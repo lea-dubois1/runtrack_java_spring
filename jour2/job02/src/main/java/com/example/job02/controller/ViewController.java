b833d401229091420a558a3197783bb6eb5706a1
lea-dubois1 <lea.dubois@laplateforme.io>
2023-09-19 10:31:51 +0200

    jobs 1 et 2 terminés

---
+++ jour2/job02/src/main/java/com/example/job02/controller/ViewController.java
@@ -0,0 +1,17 @@
+package com.example.job02.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/")
+    public String getPage() {
+        return "view";
+    }
+    @GetMapping("/aurevoir")
+    public String getAurevoir() {
+        return "test";
+    }
+}
