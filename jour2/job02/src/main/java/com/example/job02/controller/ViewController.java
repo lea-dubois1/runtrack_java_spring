4fbbb0656af49e57d0579bebccf2fe48769d9481
lea-dubois1 <lea.dubois@laplateforme.io>
2023-09-19 11:00:32 +0200

    job 2 terminé

--- jour2/job02/src/main/java/com/example/job02/controller/ViewController.java
+++ jour2/job02/src/main/java/com/example/job02/controller/ViewController.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ViewController {
 
+    public String message = "Bonjour toi";
+
     @GetMapping("/")
     public String getPage() {
         return "view";
+    }
+    public String getMessage() {
+        return this.message;
     }
     @GetMapping("/aurevoir")
     public String getAurevoir() {
